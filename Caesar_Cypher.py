94f5a9da7bf43c424f21b9205d222591b841f615
Zeomite <105901968+Zeomite@users.noreply.github.com>
2022-10-08 19:42:36 +0530

    Create Caesar_Cypher.py

---
+++ Caesar_Cypher.py
@@ -0,0 +1,42 @@
+def ceaser_cipher(message,shift_number,choice):
+    alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    output=""
+    for i in message:
+        if i in alpha:
+            if choice==1:
+                z=alpha.index(i)+shift_number
+                if z>25:
+                    z=z-26
+                output+=alpha[z]
+            else:
+                z=alpha.index(i)-shift_number
+                output+=alpha[z]
+        else:
+            output+=i
+    print(output)
+    
+choice=int(input("""           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+
+Choose 1 or 2:
+1)Encrypt
+2)Decrypt
+"""))
+message=input("Enter message:\n").lower()
+shift_number=int(input("Enter shift_number: "))
+ceaser_cipher(message,shift_number,choice)
