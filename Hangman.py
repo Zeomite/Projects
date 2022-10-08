bc74b71b2d6a5187dea2a1f5a8ff62256f77eca6
Zeomite <105901968+Zeomite@users.noreply.github.com>
2022-10-08 14:27:08 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,20 +1,24 @@
 word= list("Beekeeper".lower())
 guessed=[]
-string=""
+string="_ _ _ _ _ _ _ _ _"
 count=0
+print(string)
 while count<=7:
-    inp= input("Enter letter: ")
-    if inp.lower() in word:
+    inp= input("Enter letter: ").lower()
+    if inp in word and inp not in guessed:
         guessed.append(inp)
     else:
         count+=1
+    string=""
     for i in word:
         if i in guessed:
             string+=i
         else:
-            string+="_"
+            string+=" _"
+    print(string)
     if not "_" in string:
         print("You win!")   
         break 
     if count==7:
         print("You Lose!")
+        break
